059d129cccac27831c7116b7a2118693aced08f8
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-24 21:05:48 +0530

    updated code

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
-// write code for Welcome component here
+import React from 'react';
+  
+  const Welcome = ({name}) =>  {
+	return (
+	  <div>
+        <h1>Hey ! {name}</h1>
+        <h2>Welcome to School.</h2>
+        <p>I hope you will achieve whatever you wish in coming years.</p>
+	  </div>
+	);
+  }
+  
+  export default Welcome;
+  
