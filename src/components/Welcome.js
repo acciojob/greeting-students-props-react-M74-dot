504a7aaaadcc9167b804c387846d031de6eabcef
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-24 21:29:19 +0530

    updated code

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,6 @@ import React from 'react';
 	  <div>
         <h1>Hey ! {name}</h1>
         <h2>Welcome to School.</h2>
-        <p>I hope you will achieve whatever you wish in coming years.</p>
 	  </div>
 	);
   }
