68d64dcc10b573ec1f105d5ede7dad00db8e84b2
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-09-24 21:46:47 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,7 @@ import React from 'react';
 	return (
 	  <div>
         <h1>Hey ! {name}</h1>
-        <p>Welcome to School.</p>
-        <h1>Hey ! {name}</h1>
-        <p>Welcome to School.</p>
+        <h2>Welcome to School.</h2>
 	  </div>
 	);
   }
